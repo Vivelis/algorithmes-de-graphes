f2d58169c3266a5f2dd1086acefe674772164615
Macece <maceo.jalbert@gmail.com>
2021-05-04 21:46:29 +0200

    début recherche_chemin et correction nomage

--- recherches_chemins.py
+++ recherches_chemins.py
@@ -1,18 +1,18 @@
 dg = {'Paris': ['Lyon', 'Toulouse', 'Rennes', 'Nancy'], 'Lyon': ['Paris', 'Marseille', 'Nice', 'Nancy'], 'Marseille': ['Lyon', 'Nice', 'Montpellier'], 'Nice': ['Marseille', 'Lyon'], 'Montpellier': ['Marseille', 'Toulouse'], 'Toulouse': ['Montpellier', 'Paris', 'Rennes'], 'Rennes': ['Toulouse', 'Paris'], 'Nancy': ['Paris', 'Lyon']}
 
-def plus_court_chemin(G, s, d):
-	"""entrée : G = liste d'adjacence; s = sommet de départ; d = sommet d'arrivée
-        retourne : Liste de sommets correspondant au trajet entre s et d"""
-    prédecesseurs = {s: None}    # dictionnaire des prédecesseurs
+def plus_court_chemin(Graphe, depart, arrivee):
+	"""entrée : Graphe = liste d'adjacence; depart = sommet de départ; arrivee = sommet d'arrivée
+        retourne : Liste de sommets correspondant au trajet entre depart et arrivee"""
+    prédecesseurs = {depart: None}    # dictionnaire des prédecesseurs
     f = []    # f = CreerFile();
-    f.append(s)    # f.enfiler(s);
-    marqués = [s]    # marquer(s);
+    f.append(depart)    # f.enfiler(depart);
+    marqués = [depart]    # marquer(depart);
     while f:    # TANT QUE la file est non vide
-        s = f.pop(-1)    # On récupère le noeud
-        for t in G[s]:    # POUR TOUT voisin t de s dans G
-            if t == d:
-                ville = s    # Destination trouvée, on remonte le chemin
-                chemin = [d]
+        depart = f.pop(-1)    # On récupère le noeud
+        for t in Graphe[depart]:    # POUR TOUT voisin t de depart dans Graphe
+            if t == arrivee:
+                ville = depart    # Destination trouvée, on remonte le chemin
+                chemin = [arrivee]
                 while ville:
                     chemin.append(ville)
                     ville = prédecesseurs[ville]
@@ -21,10 +21,14 @@ def plus_court_chemin(G, s, d):
             elif t not in marqués:    # SI t non marqué
                 f.append(t)    # f.enfiler(t);
                 marqués.append(t)    # marquer(t);
-                prédecesseurs[t] = s    # màJ du dictionnaire de prédecesseurs 
+                prédecesseurs[t] = depart    # màJ du dictionnaire de prédecesseurs 
     return []# Destination non trouvée  
                 
 print("Chemin le plus court")
 print("---------------------------------------")
 print(plus_court_chemin(dg,"Nice","Toulouse"))
 print("-----------------------------------")
+
+def recherche_chemin(Graphe, depart, arrivee):
+	"""entrée : Graphe = liste d'adjacence, depart = sommet de départ, arrivee = sommet d'arrivée
+		retourne : Liste de sommets correspondant au chemin entre depart et arrivee"""
